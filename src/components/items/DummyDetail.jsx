e560a1c64eb10d0cec0172a965826f202ef98538
Elise Muellerleile <eliamue@live.com>
2022-03-19 22:57:43 -0500

    Detail page now renders dummy data dynamically

--- src/components/items/DummyDetail.jsx
+++ src/components/items/DummyDetail.jsx
@@ -1,22 +1,30 @@
-import React from 'react';
+import React from "react";
+import { useParams } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
+import useDummyData from "../../services/dummyService";
 
 export default function DummyDetail() {
+  const { id } = useParams();
+  const { dumbObject, loading } = useDummyData(id);
+
+  if (loading) return <h1>Loading...</h1>;
+
   return (
     <>
-    <figure className="item-figure">
-      <h1 className="name">A Title</h1>
-      <a href="https://www.zombo.com/" alt="A Title">
-        <img
-          src="https://thiscatdoesnotexist.com/"
-          className="logo"
-          alt="A Title"
-        />
-      </a>
-      <section className="description">Number 1 is A Title</section>
-      <section className="details">A Title is truthy thing!</section>
-    </figure>
-    <HashLink to={"/list"}>Back to list</HashLink>
-  </>
-  )
+      <figure className="figure-container">
+          <figcaption className="title">{dumbObject.title}</figcaption>
+        <a href={dumbObject.website} className="item-link">
+          <img
+            className="item-img"
+            alt={dumbObject.title}
+            src={dumbObject.logo}
+          />
+        </a>
+        <div className="textfield">{dumbObject.textfield}</div>
+        <div className="truthy">{dumbObject.truthy_thing}</div>
+        <div className="number">{dumbObject.number_thing}</div>
+      </figure>
+      <HashLink to={"/list"}>Back to list</HashLink>
+    </>
+  );
 }
